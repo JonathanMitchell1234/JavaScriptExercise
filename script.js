8f02fea1d4c8b0216f1aabde725749327d2d2862
Jonathan Mitchell <jonmitchell1234@gmail.com>
2021-09-20 21:31:46 -0400

    JavaScriptExercise

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("13");
+
+// Numbers, Strings, Booleans, Null, Undefined
+
+// Parentheses, Exponents, Multiplication, Division, Modulus, Addition/Subtraction
+
+console.log(5 - 4);
+
+console.log(6 - 5);
+
+console.log("I'm Mary Poppins, Y'all!");
+
+console.log("Hawk" + "eye");
+
+console.log("Jonathan Mitchell".length);
+
+console.log("Avengers: Endgame"[16]);
