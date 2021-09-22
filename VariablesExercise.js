f59f2b750f6e9248caab3767bbac112668c306c8
Jonathan Mitchell <jonmitchell1234@gmail.com>
2021-09-22 09:52:58 -0400

    finished variables exercise

---
+++ VariablesExercise.js
@@ -0,0 +1,33 @@
+// // 1
+// const 
+
+
+// 2
+let num = 20;
+console.log(num);
+
+// 3
+num = 15;
+console.log(num1);
+
+// 4
+let num  = 10 + 7;
+console.log(num);
+
+// 5
+
+num--
+
+
+// 6
+num = "This is not a number";
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+console.log(blank);
